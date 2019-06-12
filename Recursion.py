646a18a87519dc363ca72609f92dfa5fdbd9dd7c
Jacob okoro <jaycobokoro@gmail.com>
2019-06-12 15:00:04 +0100

    Recursion algos, including solutions to MITx problem sets
    Signed-off-by: Jacob okoro <jaycobokoro@gmail.com>

--- Recursion.py
+++ Recursion.py
@@ -33,57 +33,40 @@ def gcdIter(first, second):
     iteratively reduce by 1 until test divides both a and b without remainder """
     test = first if first < second else second
     while test >=1:
-        if first % test==0 and second%test==0:
+        if first % test==0 and second % test == 0:
             return test
         else:
             test-=1
-    return False
+    return 1
     
 
-    
-
-print(gcdIter(1000, 100))
-
-
-
-
+def gcdRecur(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcdRecur(b, a%b)
 
 
+def toChars(s):
+    s = s.lower()
+    word = ''
+    # alpha = 'abcdefghijklmnopqrstuvwxyz'
+    for letter in s:    
+        if letter in 'abcdefghijklmnopqrstuvwxyz':
+            word+=letter
+    return word
 
 
+def isPal(s):
+    charword = toChars(s)
+    if len(s) <= 1:
+        return True
+    else:
+        return s[0] == s[-1] and isPal(s[1:-1])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+s = 'aba aba'
+print(isPal(s))
+# print(gcdRecur(10, 67))
 
 # def iterPower(base, exp):
 #     result = 1
